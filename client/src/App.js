e836e7c5c90e7b3c22a6306dfce7ee4643ae9ee7
Decagon <decagon@Decagons-MacBook-Air-Usha.local>
2022-08-17 15:27:50 +0100

    first commit

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+import Login from './components/Login'
+import Signup from './components/Signup'
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+      <Signup />
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/components/Signup.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const signup = () => {
+    return (
+        <div>
+            <h1>This is signup page</h1>
+        </div>
+    );
+};
+
+export default signup;
